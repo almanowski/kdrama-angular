7d71804bb618936775d458d466d5e40536ca242d
Anna Manowski <anna.l.manowski@gmail.com>
2022-03-04 19:56:27 +0100

    Create detail-view

---
+++ src/app/detail-view/detail-view.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-detail-view',
+  templateUrl: './detail-view.component.html',
+  styleUrls: ['./detail-view.component.scss']
+})
+export class DetailViewComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DetailViewComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {drama: any}
+  ) { }
+  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
