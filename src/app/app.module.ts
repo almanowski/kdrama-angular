ae967e740cf3b7072c716c12bb74de3b6875fee1
Anna Manowski <anna.l.manowski@gmail.com>
2022-03-04 19:53:37 +0100

    Add Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,14 +18,44 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { DramaCardComponent } from './drama-card/drama-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+
+
+// App routes
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent},
+  { path: 'dramas', component: DramaCardComponent},
+  { path: 'profile', component: ProfileViewComponent},
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
-    UserLoginFormComponent
+    UserLoginFormComponent,
+    DramaCardComponent,
+    WelcomePageComponent,
+    DirectorViewComponent,
+    GenreViewComponent,
+    DetailViewComponent,
+    NavigationComponent,
+    ProfileViewComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +68,14 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatCardModule,
     MatFormFieldModule,
     MatSnackBarModule,
-    MatInputModule
+    MatInputModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
