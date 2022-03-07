65169ff085bbcdf602ee0ce036b5d3d0a8cc3476
Anna Manowski <anna.l.manowski@gmail.com>
2022-03-07 17:23:26 +0100

    Add dialog and snackbar

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { UserUpdateFormComponent } from '../user-update-form/user-update-form.component';
 
 @Component({
   selector: 'app-profile-view',
@@ -13,52 +17,59 @@ export class ProfileViewComponent implements OnInit {
 
   constructor(
     public fetchApiData: FetchApiDataService,
-    public router: Router
+    public router: Router,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     this.getCurrentUser();
-    /* this.getFavDramas(); */
+    this.getFavDramas();
   }
 
   getCurrentUser(): void {
     this.fetchApiData.getUser().subscribe((resp: any) => { 
       this.user = resp;
-      /* this.favs = resp.FavDramas; */
-      console.log(this.user);
-      return (this.user/* , this.favs */);
+      return (this.user);
     });
   }
 
-/*   getFavDramas(): void {
-    let dramas: any[] = [];
-    this.fetchApiData.getAllDramas().subscribe((resp: any) => { 
-      dramas = resp;
-      let fav = this.user.FavDramas
-      fav && dramas.map((dramas) => {
-        if ( dramas._id === fav.find((fav:any) => fav === dramas._id)) {
-          return (fav)
-        }
-        console.log(dramas)
-      })
-    })     
-  } */
+  openUserUpdateDialog(): void {
+    this.dialog.open(UserUpdateFormComponent, {
+      width: '280px'
+    }).afterClosed().subscribe(()=> this.ngOnInit());
+  }
 
+  getFavDramas(): void {
+    this.fetchApiData.getAllDramas().subscribe((resp: any) => { 
+      let favDramaId = this.user.FavDramas
+      this.favs = resp.filter((d:any) => {
+        return favDramaId.indexOf(d._id) >=0;
+      });
+    })   
+  }
 
   deleteUserFav(id: string): void {
     this.fetchApiData.deleteFavDrama(id).subscribe((resp: any) => {
-      window.location.reload();
+      this.ngOnInit();
     });
   }
 
   deleteUser(): void {
     if (confirm('Are you sure?')) {
-      this.router.navigate(['Welcome'])
+      this.fetchApiData.deleteUser().subscribe(() => {
+        localStorage.clear();
+      });
+      this.snackBar.open(this.user.Username, 'Account successfully deleted!', {
+        duration: 2000,
+        panelClass: 'action-del',
+        verticalPosition: 'top',
+        horizontalPosition: 'center'
+      });
+      setTimeout(() => {
+        this.router.navigate(['/welcome'])
+      }, 2000)    
     }
-    this.router.navigate(['Welcome'])
-    this.fetchApiData.deleteUser().subscribe(() => {
-      localStorage.clear();
-    });
   }
 
 }
